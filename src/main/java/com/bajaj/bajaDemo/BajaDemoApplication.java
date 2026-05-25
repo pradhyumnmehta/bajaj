d1e6d4d2a7c8be3f53adbedabee61073c08cc4fb
pradhyumnmehta <pradhyumnmeht29905@gmail.com>
2026-05-25 23:31:01 +0530

    first commit

---
+++ src/main/java/com/bajaj/bajaDemo/BajaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bajaj.bajaDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BajaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bajaj/bajaDemo/BajaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajaj.bajaDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
